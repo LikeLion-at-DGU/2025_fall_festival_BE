c940272eeb61f2595822bf7e220f122980d0b468
yunheewon <yunheewon66@gmail.com>
2025-09-17 11:53:30 +0900

    Feat: stage 모델 수정

--- stage/models.py
+++ stage/models.py
@@ -3,5 +3,25 @@ from django.db import models
 # Create your models here.
 class Stage(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)  #필요한가
+    # booth 앱의 Location 모델 참조
+    location = models.ForeignKey('booth.Location', on_delete =models.CASCADE, related_name='stages')
+    
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    image_url = models.URLField(blank=True, null=True)
+
     is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta: 
+        ordering = ['start_time']
+        indexes = [
+            models.Index(fields=['start_time']),
+            models.Index(fields=['end_time']),
+            models.Index(fields=['location']),
+            models.Index(fields=['is_active']),
+            models.Index(fields=['name'])
+        ]
