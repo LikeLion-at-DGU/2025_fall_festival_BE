c76aa804b5de98c8e03df0a82760ba1e534ce065
yunheewon <yunheewon66@gmail.com>
2025-09-18 01:39:06 +0900

    Feat/#13/stagelist

--- stage/serializers.py
+++ stage/serializers.py
@@ -3,17 +3,16 @@ from .models import Stage
 
 class StageSerializer(serializers.ModelSerializer):
     location_name = serializers.CharField(source="location.name", read_only=True)
-    is_live_now = serializers.SerializerMethodField()
+    is_active = serializers.SerializerMethodField()
 
     class Meta:
         model = Stage
         fields = [
             "id", "name", "start_time", 
-            "end_time", "is_live_now", 
+            "end_time", "is_active", 
             "image_url", "location_name"
         ]
 
     def get_is_live_now(self, obj):
         return obj.is_live_now
-    
     
