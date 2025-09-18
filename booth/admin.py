2b6e16269938418e65f1c9b45501e0b82bc93e15
hairyung2002 <hairyung2002@naver.com>
2025-09-18 09:27:42 +0900

    🐛 Fix:부스 리스트 Admin

--- booth/admin.py
+++ booth/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Booth)
+admin.site.register(BoothDetail)
+admin.site.register(Menu)
+admin.site.register(BoothSchedule)
+admin.site.register(Like)
