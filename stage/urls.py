bb98e865e478e3b7fa02b602998b967150dd3076
yunheewon <yunheewon66@gmail.com>
2025-09-19 08:45:19 +0900

    chore: 마이그레이션 파일 정리

--- stage/urls.py
+++ stage/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import StageViewSet
 
 router = DefaultRouter()
-router.register(r"stages", StageViewSet, basename="stage")
+router.register(r"", StageViewSet, basename="stage")
 
 urlpatterns = [
     path("", include(router.urls)),
