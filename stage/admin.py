e94615bafb1aaef73a30ffb7605b6e5fe14f23a5
yunheewon <yunheewon66@gmail.com>
2025-09-19 08:49:14 +0900

    fix: url 수정

--- stage/admin.py
+++ stage/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Stage
+
+@admin.register(Stage)
+class StageAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "location", "start_time", "end_time", "is_active","image_url")
+    list_filter = ("location", "start_time")
+    search_fields = ("name",)
+    ordering = ("start_time",)
