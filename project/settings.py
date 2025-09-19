2feb5d71e8dfd0abd3a50898cacc05f649c16e11
hairyung2002 <hairyung2002@naver.com>
2025-09-19 16:51:23 +0900

    🐛 Fix:부 부스 좋아 부스 좋아요 통일(#69)

--- project/settings.py
+++ project/settings.py
@@ -205,6 +205,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "https://2025fallfestivaldgu.netlify.app",
+    "https://dgu-fallfesta.site",
 ]
 
 # AWS_STORAGE_BUCKET_NAME = "dummy-bucket"
